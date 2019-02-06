c6d787166dd40cbea4f678fd5c91b0fbc7406f4a
adityakaria <adityakaria01@gmail.com>
2019-02-06 22:43:06 +0530

    Added KT

--- daa/labs/lab2/peak.cpp
+++ daa/labs/lab2/peak.cpp
@@ -5,12 +5,24 @@ void findPeak(vector<int> array, int begin, int end);
 
 int main(void) {
     int i, j;
-    vector<int> array{ 2, 3, 5, 7, 36, 43, 9, 1, 0, -4, -11 };
+    // vector<int> array;
+    int arr[] = {2, 3, 5, 7, 36, 43, 9, 1, 0, -4, -11}; 
+    int n = sizeof(arr) / sizeof(arr[0]); 
+    vector<int> array(arr, arr + n); 
+    // for (int x = 0; x < n; x++) {
+    //     array.push_back(arr[x]);
+    //     cout << arr[x] << " ";
+    // }
+    for (int x = 0; x < array.size(); x++) 
+        cout << array[x] << " "; 
+    cout << endl;
+    // vector<int> *array = array{ 2, 3, 5, 7, 36, 43, 9, 1, 0, -4, -11 };
     findPeak(array, 0, array.size());
 
 }
 
 void findPeak(vector<int> array, int begin, int end) {
+    cout << "\t1" << endl;
     int middle = (begin + end) / 2;
     if (array[middle - 1] < array[middle]) {
         findPeak(array, middle, end);
