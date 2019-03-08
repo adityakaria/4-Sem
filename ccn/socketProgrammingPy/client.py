89fb0e4d24297c1e6ab213bdddc2a1ceef3a5e09
adityakaria <adityakaria01@gmail.com>
2019-03-08 12:54:07 +0530

    Before CCN Lab exam

--- ccn/socketProgrammingPy/client.py
+++ ccn/socketProgrammingPy/client.py
@@ -1,19 +1,42 @@
+# import socket
+
+# s = socket.socket()
+
+# dnsTable = {'www.google.com':'92.424.6.1', 'www.apple.com':'123.45.56.78', 'www.abcd.com':'0.0.0.0'}
+
+# port = 8090
+# s.connect(('127.0.0.1', port))
+
+# print('available options:\n1. www.google.com\n2. www.apple.com\n3.www.abcd.com\n')
+
+# blah=input("Enter the address:").encode('utf-8')
+# # blah = 'blah'
+# # print('---1')
+# s.send(blah)
+# # print('---2')
+# print(s.recv(1024).decode('utf-8'))
+
+# s.close()
+
 import socket
+import os, sys
 
-s = socket.socket()
+HOST = '127.0.0.1'
+PORT = 5000
 
-dnsTable = {'www.google.com':'92.424.6.1', 'www.apple.com':'123.45.56.78', 'www.abcd.com':'0.0.0.0'}
+s= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-port = 8090
-s.connect(('127.0.0.1', port))
+try:
+    s.connect((HOST, PORT))
+    while True:
+        string_to_send = str(input("You: "))
+        s.sendall(string_to_send.encode('utf-8'))
+        # data = s.recv(1024).decode('utf-8')
+        # if not data:
+            # sys.exit()
+        # print("Server: ", data)
+except socket.error as err:
+    print("Socket Error as", err)
+finally:
+    a.close()
 
-print('available options:\n1. www.google.com\n2. www.apple.com\n3.www.abcd.com\n')
-
-blah=input("Enter the address:").encode('utf-8')
-# blah = 'blah'
-# print('---1')
-s.send(blah)
-# print('---2')
-print(s.recv(1024).decode('utf-8'))
-
-s.close()
--- ccn/socketProgrammingPy/server.py
+++ ccn/socketProgrammingPy/server.py
@@ -1,21 +1,49 @@
+# import socket
+
+# s = socket.socket()
+# print("Socket created")
+# dnsTable = {'www.google.com':'92.424.6.1', 'www.apple.com':'123.45.56.78', 'www.abcd.com':'0.0.0.0'}
+
+# port = 8090
+
+# s.bind(('', port))
+# print('socket binded to', port)
+
+# s.listen(5)
+# print('socket is now listening..')
+
+# while True:
+#     c, addr = s.accept()
+#     print('got connection from', addr)
+#     address = (c.recv(1024).decode('utf-8'))
+#     print(address)
+#     c.send(dnsTable[address].encode('utf-8'))
+#     c.close()
+
 import socket
+import os, sys
 
-s = socket.socket()
-print("Socket created")
-dnsTable = {'www.google.com':'92.424.6.1', 'www.apple.com':'123.45.56.78', 'www.abcd.com':'0.0.0.0'}
+a = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-port = 8090
+HOST = '127.0.0.1'
+PORT = 5000
+file = open('hello.txt', 'w')
 
-s.bind(('', port))
-print('socket binded to', port)
-
-s.listen(5)
-print('socket is now listening..')
-
-while True:
-    c, addr = s.accept()
-    print('got connection from', addr)
-    address = (c.recv(1024).decode('utf-8'))
-    print(address)
-    c.send(dnsTable[address].encode('utf-8'))
-    c.close()
+try:
+    a.bind((HOST, PORT))
+    a.listen()
+    conn, addr = a.accept()
+    with conn:
+        while True:
+            data = conn.recv(1024).decode('utf-8')
+            if not data:
+                sys.exit()
+            else:
+                # print("Client: ", data)
+                file.write(data)
+            # string_to_send = str(input("You: "))
+            # conn.send(string_to_send.encode('utf-8'))
+except socket.error as err:
+    print("Socket Error as", err)
+finally:
+    # a.close()
